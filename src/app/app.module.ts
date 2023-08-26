423fcd4e24ff3386f20f7892b70cacb5906c1ab0
rihabgmd <riheb.gammoudi@isimg.tn>
2023-08-26 10:35:28 +0100

    corection carossele css

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FeatureComponent } from './feature/feature.component';
+import { AboutComponent } from './about/about.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FeatureComponent,
+    AboutComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
